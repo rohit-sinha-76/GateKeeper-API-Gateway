e1011337cb74f785f2bc2a02cb4f56d71cbf9543
rohit sinha <rohitsinha0311@gmail.com>
2026-07-16 14:00:00 +0530

    feat(middleware): add Gzip compression middleware for gateway responses

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 from core.config import settings
 from utils.logger import get_logger
@@ -30,6 +31,7 @@ app = FastAPI(
 
 # Middleware
 app.add_middleware(TracingMiddleware)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
